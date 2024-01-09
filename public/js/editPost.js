2256f0c8614f69e6443dea443d4435451725336e
Amanuel <amanueleman@gmail.com>
2024-01-08 23:10:08 -0500

    functions to add new comment and edit post are created

---
+++ public/js/editPost.js
@@ -0,0 +1,19 @@
+let post = window.location.pathname.split("/");
+
+// Function allowing user to edit posts
+const editPost = async (event) => {
+  event.preventDefault();
+
+  // get text and trim whitespace
+  const comment_body = document.getElementById("edit-btn").value.trim();
+
+
+  document.location.assign(`/create/${post[2]}`);
+};
+
+const editButton = document.querySelectorAll("#edit-btn");
+
+// Iterates over all buttons on the page allowing for edit functionality
+for (let i = 0; i < editButton.length; i++) {
+  editButton[i].addEventListener("click", editPost);
+}
